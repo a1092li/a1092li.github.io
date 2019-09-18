136ed4485966682fc76f4e085b8a5d7a5e9f5a6b
a1092li <55456788+a1092li@users.noreply.github.com>
2019-09-18 10:32:38 +0300

    Site v1

---
+++ Test1Site/scripts/main.js
@@ -0,0 +1,13 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/cat1.jpg') {
+      myImage.setAttribute ('src','images/cat2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/cat1.jpg');
+    }
+}
