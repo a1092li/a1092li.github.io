2e0cee9cb0f52e224b0895060ed0f8d7a4b56504
a1092li <55456788+a1092li@users.noreply.github.com>
2019-09-18 15:24:04 +0300

    doeSupport v1

--- Test1Site/scripts/main.js
+++ Test1Site/scripts/main.js
@@ -1,20 +1,9 @@
+// Beginning of execution
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
 var myImage = document.querySelector('img');
 var x = document.getElementById("geolocation");
-function KnowMyGeo() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}  
-
-function ClearMyGeo() {
-  x.innerHTML = "<br>Latitude: ..." +
-  "<br>Longitude: ...<br>";
-}  
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
@@ -25,8 +14,27 @@ myImage.onclick = function() {
     }
 }
 
+
+if (window.DeviceOrientationEvent) {
+  window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+  document.getElementById("doeSupported").innerText = "..YES..";
+}
+
+// There are only functions below
+
+function KnowMyGeo() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}  
+
+function ClearMyGeo() {
+  x.innerHTML = "<br>Latitude: ...<br>Longitude: ...<br>";
+}  
+
 function showPosition(position) {
   x.innerHTML = "<br>Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude+"<br>";
 }
-
