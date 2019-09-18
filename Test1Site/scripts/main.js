fbae46cd699ff5ea86051127498cc258803ce761
a1092li <55456788+a1092li@users.noreply.github.com>
2019-09-18 14:40:47 +0300

    Version with GeoLocation

--- Test1Site/scripts/main.js
+++ Test1Site/scripts/main.js
@@ -2,6 +2,19 @@ var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
 var myImage = document.querySelector('img');
+var x = document.getElementById("geolocation");
+function KnowMyGeo() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}  
+
+function ClearMyGeo() {
+  x.innerHTML = "<br>Latitude: ..." +
+  "<br>Longitude: ...<br>";
+}  
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
@@ -11,3 +24,9 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/cat1.jpg');
     }
 }
+
+function showPosition(position) {
+  x.innerHTML = "<br>Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude+"<br>";
+}
+
