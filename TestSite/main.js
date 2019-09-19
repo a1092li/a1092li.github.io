63a0b5a19f358ef3942645b6a62c8a31049e5037
a1092li <55456788+a1092li@users.noreply.github.com>
2019-09-19 10:26:12 +0300

    v7

--- TestSite/main.js
+++ TestSite/main.js
@@ -1,10 +1,10 @@
 /*if(window.DeviceOrientationEvent) { //Do something }*/
 window.addEventListener('deviceorientation', function(event) {
-  var alpha = event.alpha;
+/*  var alpha = event.alpha;
   var beta = event.beta;
-  var gamma = event.gamma;
+  var gamma = event.gamma;*/
   // Do something
-  alert(event.alpha);
-  alert(event.beta);
-  alert(event.gamma);
+  //alert(event.alpha);
+  //alert(event.beta);
+  //alert(event.gamma);
 }, false);
